be47ec93a69fd152a58c93597a8bbc1f42328d66
Tomas Petras Rupsys <tprupsys@gmail.com>
2014-03-07 00:47:29 +0200

    Destytojo reikalavimai surasyti i komentara

--- operating_system.c
+++ operating_system.c
@@ -11,6 +11,17 @@
 /** The beginning */
 int main(void) {
 	puts("OS2 Deadline: kovo 28!");
+	
+	// Pagal destytojo ant lentos brezta konspekta:
+	// Loader() -> byte[] is failo
+	// while (true) {
+	//   komandos dekodavimas (atmintis per puslapiavima)
+	//   komandos vykdymas (tik CPU, atmintis)
+	//   kanalu irenginys
+	//   timer
+	//   pertraukimu tikrinimas (supervizoriaus rezimas)
+	// }
+	
 	std::getchar();
 	return EXIT_SUCCESS;
 }
