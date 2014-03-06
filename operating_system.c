7b839267807c98a76f0b4df9bbed0eebbf5cff51
Tomas Petras Rupsys <tprupsys@gmail.com>
2014-03-07 00:37:33 +0200

    Pridedu visa veikimo mechanizma. create_os.bat kompiliuoja os.exe pagal operating_system.c. Viskas pritaikyta windows g++ (MinGW)

---
+++ operating_system.c
@@ -0,0 +1,16 @@
+/*
+ ============================================================================
+	Tomas Petras Rupðys ir Dominykas Ðioþinis
+	Operaciniø Sistemø atsiskaitymo uþduotis
+ ============================================================================
+ */
+#include <cstdio>
+#include <stdio.h>
+#include <stdlib.h>
+
+/** The beginning */
+int main(void) {
+	puts("OS2 Deadline: kovo 28!");
+	std::getchar();
+	return EXIT_SUCCESS;
+}
